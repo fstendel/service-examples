cf0fbf98156c145db5b63a0b967584d26bef758b
Florian Stendel <florian.stendel.apache@gmx.de>
2017-06-16 08:50:13 +0200

    Added simple Testcase

---
+++ src/test/java/de/florianstendel/apps/soap/EchoServiceTest.java
@@ -0,0 +1,37 @@
+package de.florianstendel.apps.soap;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by fstendel on 16.06.2017.
+ */
+public class EchoServiceTest {
+
+    private EchoService echoService;
+
+
+    @Before
+    public void setUp(){
+
+        echoService = new EchoService();
+
+    }
+
+
+    @Test
+    public void testEchoEchoes(){
+
+        // Given
+        final String input = "echoIn";
+        final String expectedOutput = "echoIn";
+
+        // When
+        final String actualOutput = echoService.echo(input);
+
+
+        //Then
+        Assert.assertEquals("Returned value expected to match input value.",expectedOutput,actualOutput);
+    }
+}
